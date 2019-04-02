70036f34bf05cd4b515e331e78bd0b2c57daef17
Allison Howard <alliemhow@gmail.com>
2019-04-02 12:31:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,6 +30,8 @@ class App < Sinatra::Base
     number2 = params[:number2].to_i
     if operation == "add"
       answer = (number1 + number2)
+    elsif operation == "subtract"
+      answer = (number1 - number2)
     end
     answer.to_s
   end
