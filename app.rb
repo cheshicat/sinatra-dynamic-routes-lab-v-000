738859de9e81bb6788d635c98462ae897bc5e1a5
Allison Howard <alliemhow@gmail.com>
2019-04-02 11:52:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,6 +14,9 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @num = params[:number].to_i
     @phrase = params[:phrase]
+    until "#{@phrase.count}" == "#{@num}"
+      "#{@phrase}"
+    end
   end
 
 end
